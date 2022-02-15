a1a26dfff708ec85445bc8626d62b1d4922c98dd
이 영 종 <naki5087@naver.com>
2022-02-15 16:51:08 +0900

    화면 이동

---
+++ app/src/main/java/com/nepplus/intent_20220215/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.nepplus.intent_20220215
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveToOtherActivity.setOnClickListener {
+//            OtherActivity로 이동
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/nepplus/intent_20220215/OtherActivity.kt
@@ -0,0 +1,13 @@
+package com.nepplus.intent_20220215
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+
+    }
+}
